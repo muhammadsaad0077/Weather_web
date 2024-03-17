53793138c5924560c98fe09a93874c0cac803754
muhammadsaad0077 <saaditanoli12@gmail.com>
2024-03-17 16:18:33 +0500

    responsiveness and more features

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,31 +2,52 @@ import React, { useEffect, useState } from 'react'
 import Display from './Display'
 
 const Body = () => {
-    const [live, setLive] = useState([])
-    const [latitude, setLatitude] = useState(30.3753); // 
-    const [longitude, setLongitude] = useState(69.3451);
-    const handleSearch = async()=>{
-        const data = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,is_day,rain&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=auto`)
-        const json = await data?.json()
-        setLive(json)
+    const [live, setLive] = useState([])  // For storing json
+    const [location, setLocation] = useState("Pakistan");
 
-    }
+    const handleSearch = async () => {   
+        
+            const url = `https://nominatim.openstreetmap.org/search?format=json&q=${location}`;  // passing entered location name 
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if (data && data.length > 0) {
+                const { lat, lon } = data[0];       // setting the value of langitude and latitude of particular location
+                const weatherData = await fetchWeatherData(lat, lon);       // Passing latitiude and longitude to fetch weather data and calling the function on onclick
+                setLive(weatherData);  // setting the weather data to the live state variable
+            }
+    };
+
+    const fetchWeatherData = async (lat, lon) => {
+        const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,is_day,rain&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=auto`;
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+    };
     useEffect(()=>{
         handleSearch()
        
     }, [])
   return (
     
-    <div className="bg h-screen overflow-x-hidden">    
-    <div className="overflow-x-hidden flex">
+    <div className="bg h-screen overflow-x-hidden overflow-y-hidden">    
+    <div className="flex">
         <ul>
-            <li className="font-bold m-4 text-xl font-mono  text-white">Global Weather</li>
-            <input value={latitude} onChange={(e)=> setLatitude(e.target.value)} className="relative bottom-10 left-96 rounded-lg" placeholder="Enter Latitude "></input>
-            <input value={longitude} onChange={(e)=> setLongitude(e.target.value)} className="relative bottom-10 left-[400px] rounded-lg" placeholder="Enter Longitude "></input>
-            <button onClick={handleSearch} className="relative bottom-10 left-[420px] rounded-lg">Search</button>
-        </ul>
-    </div>
-    <Display live={live} />
+            <li className="font-bold ml-[25vw] m-2 md:mt-10 mt-[27px] md:ml-6 md:m-4 text-xl font-mono  text-white">Global Weather</li>
+            <input 
+    value={location} 
+    onChange={(e)=> setLocation(e.target.value)} 
+    className="relative bottom-0 md:mt-0 mt-12 md:bottom-10 left-8 md:left-[520px] w-1/2 rounded-lg md:w-4/6 px-4 py-2 bg-gray-100 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
+    placeholder="Enter Location "
+/>
+<button 
+    onClick={handleSearch} 
+    className="relative left-16 bottom-0 md:bottom-10 md:left-[540px] md:mt-0 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+>
+    Search
+</button>        </ul>
+    </div> 
+    <Display live={live} />  
     </div>
     
   )
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,44 +1,38 @@
 import React from "react";
 
-const Display = ({ live }) => {
-  /* const [info, setInfo] = useState("")
-   
-   live?.daily?.temperature_2m_min.map((minTemp)=>{
-     return setInfo(minTemp)
-   })
-   console.log(info);*/
-  console.log(live);
+const Display = ({ live }) => {    // Recieving live as props
 
   return (
 <>
-<h1 className="text-4xl font-bold h-6 ml-12 mt-28">{live?.timezone}</h1>
-      <h1 className="text-4xl font-bold ml-96 relative bottom-6">
-        {live?.current?.temperature_2m} {live?.current_units?.temperature_2m}
+<h1 className="text-4xl font-bold h-6 ml-8 md:ml-12 mt-16 md:mt-20">{live?.timezone}</h1>   {/* Current Location */}
+      <h1 className="md:text-4xl text-3xl font-bold ml-[284px] bottom-5 md:ml-96 relative md:bottom-6">
+        {live?.current?.temperature_2m} {live?.current_units?.temperature_2m}  {/* Current Temperature */}
       </h1>
-      <h1 className="-mt-3 ml-12">
+      <h1 className="md:-mt-3 md:ml-12 -mt-4 ml-8">  {/* Current date and time */}
           {live?.current?.time.substring(0, 10)}{" "}
           {live?.current?.time.substring(11, 16)}
         </h1>
-      <div className="flex relative right-[0.8%] w-[100%] h-[100%] mt-8">
-        {live?.daily?.time.map((temp, index) => {
-          const sunsetTime = live?.daily?.sunset[index]?.substring(11, 16);
-          const sunriseTime = live?.daily?.sunrise[index]?.substring(11, 16);
-          const maxTemp = live?.daily?.temperature_2m_max[index];
-          const minTemp = live?.daily?.temperature_2m_min[index];
-          const date = new Date(temp);
+      <div className="flex relative right-[0.8%] w-[100%] h-[100%] mt-8 md:flex-nowrap flex-wrap justify-center">
+        {live?.daily?.time.map((temp, index) => {           // destructuring
+          const sunsetTime = live?.daily?.sunset[index]?.substring(11, 16);  // sunset time
+          const sunriseTime = live?.daily?.sunrise[index]?.substring(11, 16);  // sunrise time
+          const maxTemp = live?.daily?.temperature_2m_max[index];  // max temperature
+          const minTemp = live?.daily?.temperature_2m_min[index];  // min temperature
+          const date = new Date(temp);  // converting data into day
           const dayOfWeek = date.getDay();
           const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-          const dayName = daysOfWeek[dayOfWeek];
+          const dayName = daysOfWeek[dayOfWeek];  // getting day according to the date
+          const unit = live?.daily_units?.temperature_2m_min;
           
           return (
-            <div className="border border-black  ml-6 w-[240px] h-[290px] mt-20 card text-black font-bold">
-              <h1 key={temp} className="ml-12">
+            <div key={temp} className="border border-black ml-6 w-[240px] h-[290px] md:mt-12 mt-20 card text-black font-bold md:text-start text-center">
+              <h1 key={temp} className="md:ml-12 ml-2">
                 {dayName}
-              </h1>
-              <h1 className="mt-8 ml-1">Max Temperature: {maxTemp}</h1>
-              <h1 className="mt-8 ml-1">Min Temperature: {minTemp}</h1>
-              <h1 className="mt-8 ml-1">Sunset: {sunsetTime}</h1>
-              <h1 className="mt-8 ml-1">Sunrise: {sunriseTime}</h1>
+              </h1>   {/* displaying data on UI */}
+              <h1 key={maxTemp} className="mt-8">Max Temperature: {maxTemp}{unit}</h1>
+              <h1 key={minTemp} className="mt-8">Min Temperature: {minTemp}{unit}</h1>
+              <h1 key={sunriseTime} className="mt-8 ml-1">Sunset: {sunsetTime}</h1>
+              <h1 key={sunsetTime} className="mt-8 ml-1">Sunrise: {sunriseTime}</h1>
             </div>
           );
         })}
